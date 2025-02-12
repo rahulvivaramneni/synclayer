38a1f38a470582625a12807525af4562441e62a9
Rahul Vivaramneni <raorahul934@gmail.com>
2025-02-12 21:36:58 +0530

    fix typos

--- src/Components/SecuritySection/Security.js
+++ src/Components/SecuritySection/Security.js
@@ -1,35 +1,40 @@
-import React from 'react'
-import Graphic from "../../Assets/Images/security-img.svg"
-import Graphic2 from "../../Assets/Images/security-img-2.svg"
+import React from "react";
+import Graphic from "../../Assets/Images/security-img.svg";
+import Graphic2 from "../../Assets/Images/security-img-2.svg";
 
 const Security = () => {
-    return (
-        <section className='security-section dark-section'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-5'>
-                        <div className='img-div'>
-                            <img src={Graphic} alt="Graphic" />
-                        </div>
-                    </div>
-                    <div className='col-lg-7'>
-                        <div className='para-content'>
-                            <h3 className='heading large'>
-                                <span>Security</span> for a User Centric <span>Future</span>
-                            </h3>
-
-                            <p className='text-white'>SyncLayer aligns with the shift toward decentralization by prioritizing user control.<br /> Security isn't imposed by a central authority; it's built collaboratively with you.</p>
-                        </div>
-                    </div>
-                    <div className='col-lg-12'>
-                        <div className='d-lg-block d-none img-div mt-5'>
-                            <img src={Graphic2} alt="Graphics" />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="security-section dark-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-5">
+            <div className="img-div">
+              <img src={Graphic} alt="Graphic" />
             </div>
-        </section>
-    )
-}
+          </div>
+          <div className="col-lg-7">
+            <div className="para-content">
+              <h3 className="heading large">
+                <span>Security</span> for a User Centric <span>Future</span>
+              </h3>
 
-export default Security
+              <p className="text-white">
+                Sync layer aligns with the shift toward decentralization by
+                prioritizing user control.
+                <br /> Security isn't imposed by a central authority; it's built
+                collaboratively with you.
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-12">
+            <div className="d-lg-block d-none img-div mt-5">
+              <img src={Graphic2} alt="Graphics" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Security;
